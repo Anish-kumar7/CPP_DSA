2b487294b9a51432e8742e1f56330345204f63b5
Anishku508 <154524681+Anishku508@users.noreply.github.com>
2024-09-08 01:50:01 +0530

    First

---
+++ Basics/code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    char grade = 'A';
+    float PI = 3.14f;
+
+    bool isSafe = true;
+    double price = 100.99;
+    cout << price << endl;
+    return 0;  
+}
