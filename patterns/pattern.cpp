9a566bafcb4bab6d4fbd2ee918cbf986408a42ac
Anishku508 <154524681+Anishku508@users.noreply.github.com>
2024-09-09 00:51:06 +0530

    Pattern Programs

---
+++ patterns/pattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for(int i=1; i<=n; i++)  { // outer loop 
+
+        for(int j=1; j<=n; j++)  {  // Inner loop 
+
+        cout << j;
+
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+
+}
