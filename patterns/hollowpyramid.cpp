c0db3c6cb0d1a2c26c69e0e9c478c46450e64b5a
Anishku508 <154524681+Anishku508@users.noreply.github.com>
2024-09-09 11:28:43 +0530

    pattern

--- patterns/hollowpyramid.cpp
+++ patterns/hollowpyramid.cpp
@@ -5,44 +5,55 @@ int main() {
 
     int n = 4;
 
-    //top 
+    // for top part 
     for(int i=0; i<n; i++) {
-        // space 
+
+        // for space printing the logic is 
         for(int j=0; j<n-i-1; j++) {
             cout << " ";
         }
+
         cout << "*";
 
-        if(i != 0) {
-            // spaces
-            for(int j=0; j<2*i-1; j++) {
-                cout << " ";
-            }
-            cout << "*";
-        }
+        if(i !=0) {
 
+
+            // for space printing in upper part after star is printted ( space > than star > space ) this will be logic 
+             for(int j=0; j<2*i-1; j++) {
+                cout << " ";
+             }
+
+             cout << "*";
+
+
+        }
         cout << endl;
 
     }
 
-    //bottom
-    for(int i=0; i<n-1; i++) {
-        //space
-        for(int j=0; j<i+1; j++){
-            cout<< " ";
-        }
 
+
+    // for bottom part logic is 
+
+    for(int i=0; i<n-i; i++) {
+
+        // for space printing 
+        for(int j=0; j<i+1; j++) {
+            cout << " ";
+
+        }
         cout << "*";
 
-        if(i != n-2){
-            //space
-            for(int j=0; j<2*(n-1)-5; j++){
+        if(i != n-2) {
+
+            // for space printing 
+            for(int j=0; j<2*(n-1)-5; j++) {
                 cout << " ";
             }
             cout << "*";
         }
         cout << endl;
     }
+
     return 0;
-
 }
